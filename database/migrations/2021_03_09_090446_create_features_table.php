a6f3b4f94d5d935c888ea2e3d231d9ddd2170fe5
Naufal Yukafi <naufalyukafi10@gmail.com>
2021-03-09 16:08:30 +0700

    feat(migration): Integrate database to feature

---
+++ database/migrations/2021_03_09_090446_create_features_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFeaturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('features', function (Blueprint $table) {
+            $table->id();
+            $table->string('image');
+            $table->text('content');
+            $table->boolean('draft')->default(false);
+            $table->dateTime('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->dateTime('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('features');
+    }
+}
