4087e6d992e1bf770018b3141621ff52a80f3933
muhammadshiva <shivamuhammad07@gmail.com>
2021-03-09 10:09:02 +0700

    feat(controller): Implement to HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,10 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Post; // Calling the model Post
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index(){ // to calling function from web.php
+        return view('index', ['posts' => Post::index()]);
+        // Return the view which is index, then create a variable 
+        // that keeps Model Post and Post's index
+
+    }
+
+    public function show() {
+        return view('video-page', ['posts' => Post::videoprev()]);
+    }
+
 }
