58ce348844053d7305aa7abf104e39001b495496
Naufal Yukafi <naufalyukafi10@gmail.com>
2021-03-11 22:47:36 +0700

    feat(migration): Integrate database to users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,8 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('Telepon', 14);
+            $table->string('level');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
