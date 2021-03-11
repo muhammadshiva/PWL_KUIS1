1f459b0fd09ab949ed1f60535896994393452baf
Naufal Yukafi <naufalyukafi10@gmail.com>
2021-03-11 11:26:49 +0700

    feat(admin): Implement root view

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,37 @@
+<div class="sidebar" data-color="purple" data-background-color="white" data-image="assetsAdmin/img/sidebar-3.jpg">
+    <div class="logo text-center">Movie Catalog</div>
+    <div class="sidebar-wrapper">
+        <ul class="nav">
+        <li class="nav-item active  ">
+            <a class="nav-link" href="./dashboard.html">
+            <i class="material-icons">dashboard</i>
+            <p>Dashboard</p>
+            </a>
+        </li>
+        <li class="nav-item ">
+            <a class="nav-link" href="./user.html">
+            <i class="material-icons">people</i>
+            <p>Pelanggan</p>
+            </a>
+        </li>
+        <li class="nav-item ">
+            <a class="nav-link" href="./tables.html">
+            <i class="material-icons">movie</i>
+            <p>Barang</p>
+            </a>
+        </li>
+        <li class="nav-item ">
+            <a class="nav-link" href="./typography.html">
+            <i class="material-icons">library_books</i>
+            <p>Supplier</p>
+            </a>
+        </li>
+        <li class="nav-item ">
+            <a class="nav-link" href="./user.html">
+            <i class="material-icons">admin_panel_settings</i>
+            <p>Pegawai</p>
+            </a>
+        </li>
+        </ul>
+    </div>
+</div>
