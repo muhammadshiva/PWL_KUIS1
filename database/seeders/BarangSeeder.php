fa1c46a4d5a984add8af1e4fefcc9b2d116a6282
muhammadshiva <shivamuhammad07@gmail.com>
2021-03-12 16:35:53 +0700

    feat(seeder): Integreate to barang

--- database/seeders/BarangSeeder.php
+++ database/seeders/BarangSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class BarangSeeder extends Seeder
 {
@@ -13,6 +14,49 @@ class BarangSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $data = [
+            [
+                'name' => 'The Fast and The Furious 8',
+                'harga' => 'Rp. 50.000,.',
+                'stock' => '3'
+            ],
+            [
+                'name' => 'Tom and Jerry',
+                'harga' => 'Rp. 60.000,.',
+                'stock' => '4'
+            ],
+            [
+                'name' => 'Nara & The Dragon',
+                'harga' => 'Rp. 45.000,.',
+                'stock' => '2'
+            ],
+            [
+                'name' => 'Monster Hunter',
+                'harga' => 'Rp. 55.000,.',
+                'stock' => '2'
+            ],
+            [
+                'name' => 'Wonder Woman 1984',
+                'harga' => 'Rp. 70.000,.',
+                'stock' => '1'
+            ],
+            [
+                'name' => 'Red Dot',
+                'harga' => 'Rp. 30.000,.',
+                'stock' => '3'
+            ],
+            [
+                'name' => 'Outside The Wire',
+                'harga' => 'Rp. 40.000,.',
+                'stock' => '4'
+            ],
+            [
+                'name' => 'The Little Things',
+                'harga' => 'Rp. 65.000,.',
+                'stock' => '5'
+            ],
+        ];
+
+        DB::table('barangs')->insert($data);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,12 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([PostSeeder::class, TestimonialSeeder::class, FeatureSeeder::class, UserSeeder::class, SupplierSeeder::class, PelangganSeeder::class]);
+        $this->call([PostSeeder::class, TestimonialSeeder::class, FeatureSeeder::class, UserSeeder::class, SupplierSeeder::class, PelangganSeeder::class, BarangSeeder::class]);
         Post::factory(100)->create();
         Testimonial::factory(100)->create();
         Feature::factory(100)->create();
         User::factory(100)->create();
         Supplier::factory(100)->create();
         Pelanggan::factory(100)->create();
+        Barang::factory(100)->create();
     }
 }
