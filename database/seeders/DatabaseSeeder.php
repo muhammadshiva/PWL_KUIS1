db9b2fa79aab01c5770f06fcf58107e67bd78a3d
muhammadshiva <shivamuhammad07@gmail.com>
2021-03-12 15:27:29 +0700

    feat(seeder): Seed database to pelanggan

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([PostSeeder::class, TestimonialSeeder::class, FeatureSeeder::class, UserSeeder::class, SupplierSeeder::class]);
+        $this->call([PostSeeder::class, TestimonialSeeder::class, FeatureSeeder::class, UserSeeder::class, SupplierSeeder::class, PelangganSeeder::class]);
         Post::factory(100)->create();
         Testimonial::factory(100)->create();
         Feature::factory(100)->create();
         User::factory(100)->create();
         Supplier::factory(100)->create();
+        Pelanggan::factory(100)->create();
     }
 }
--- database/seeders/PelangganSeeder.php
+++ database/seeders/PelangganSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PelangganSeeder extends Seeder
 {
@@ -13,6 +14,35 @@ class PelangganSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $data = [
+            [
+                'name' => '	Muhammad Shiva',
+                'telepon' => '(021)-8494321',
+                'alamat' => 'Jl. Boulevard Kota Wisata, Jakarta'
+            ],
+            [
+                'name' => '	Naufal Yukafi Ridlo',
+                'telepon' => '(021)-6232011',
+                'alamat' => 'LTC Glodok Lt. SB Blok C1 No.10Jl, Jakarta'
+            ],
+            [
+                'name' => '	Dani Satria',
+                'telepon' => '(021)-4321111',
+                'alamat' => 'Jl. Dau, Malang'
+            ],
+            [
+                'name' => '	Dewa Fikri',
+                'telepon' => '(021)-4321222',
+                'alamat' => 'Jl. Pandan, Malang'
+            ],
+            [
+                'name' => '	Firdaus Maulana',
+                'telepon' => '(021)-4321333',
+                'alamat' => 'Jl. Kembang Kertas, Malang'
+            ],
+            
+        ];
+
+        DB::table('pelanggans')->insert($data);
     }
 }
